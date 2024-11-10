84960df63a5b02418528d86900bdeef0d0b6d390
zaini5170 <ahmadzaini9816@gmail.com>
2024-11-10 14:46:22 +0700

    cobaa

---
+++ app/Views/user_3/edit_akun.php
@@ -0,0 +1,27 @@
+ <!-- Content Wrapper. Contains page content -->
+ <div class="content-wrapper">
+     <section class="content-header">
+         <div class="container-fluid">
+             <div class="row mb-2">
+                 <div class="col-sm-6">
+                     <h1>Edit Akun</h1>
+                 </div>
+                 <div class="col-sm-6">
+                     <ol class="breadcrumb float-sm-right">
+                         <li class="breadcrumb-item"><a href="#">Home</a></li>
+                         <li class="breadcrumb-item active">Edit Akun</li>
+                     </ol>
+                 </div>
+             </div>
+         </div><!-- /.container-fluid -->
+     </section>
+     <!-- Content Header (Page header) -->
+     <section class="content-header">
+
+
+     </section>
+
+     <!-- Main content -->
+
+     <!-- /.content -->
+ </div>
